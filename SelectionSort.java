754ddc032e27f970aae9f76b7981b581af4e10cf
MiMao Da <35322256+YanWu1230@users.noreply.github.com>
2019-11-13 12:01:42 -0800

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,58 +1,25 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class SelectionSort {
+ public static void main(String[] args) {
+        int[] arr = new int[]{3, 4, 5, 1, 2};
+        selectSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 
-	public static void main(String[] args) throws FileNotFoundException {
-		String fileName = "hw3.dat";
-		Scanner s = new Scanner(new FileReader(fileName));
-		int length = s.nextInt();
-		if(length <= 0)
-			System.out.print("The file do not have any number need to be sort!");
-		else if (length == 1) {
-			int number = s.nextInt();
-			System.out.print(number);
-		}else {
-			int[] arr = new int[length];
-			for(int i = 0; i < length; i++) {
-				arr[i] = s.nextInt();
-			}
-			selectionSort(arr, length);
-			//selectionSort2(arr, length);
-			for(int i = 0; i < length; i++) {
-				System.out.print(arr[i] + " ");
-			}
-		}
-		
-	}
-	//find the maximum
-	private static void selectionSort2(int[] arr, int length) {
-		for(int i = length - 1; i > 0;i--) {
-			int max = i;
-			for(int j = i; j >= 0; j--) {
-				if(arr[j] > arr[max]) {
-  					max = j;
-				}
-			}
-			swap(arr, i, max);
-		}
-	}
-	// find the minimum
-	private static void selectionSort(int[] arr, int length) {
-		for(int i = 0; i < length - 1; i++) {
-			int min = i;
-			for(int j = i; j < length ; j++) {
-				if(arr[min] > arr[j]) {
-					min = j;
-				}
-			}
-			swap(arr, i, min);
-		}
-	}
-	private static void swap(int[] arr, int i, int j) {
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-	}
+    public static void selectSort(int[] arr) {
+        for (int i = 0; i < arr.length -1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if(arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if(minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
 }
